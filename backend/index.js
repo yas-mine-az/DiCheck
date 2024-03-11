4d77557141f4061f01b2066cc84f925f1a917547
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2024-03-11 13:43:57 +0700

    feat: middleware

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,22 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const medicalRecordRoutes = require('./routes/medicalRecordRoutes');
 const port = 3000;
 
 app.use(cors());
+app.use(express.json()); // This should be placed before your routes
+
 app.use("/api/user", userRoutes);
 app.use("/api/record", medicalRecordRoutes);
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  console.log(`Received ${req.method} request to ${req.url}`);
+  next();
+});
 
 mongoose
   .connect(process.env.MONGO_URI, {

4b111535683cc42065d959279a4546b5a670afc2
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2024-05-24 01:04:37 +0700

    feat: create article page

---
+++ frontend/pages/articles/index.js
@@ -0,0 +1,82 @@
+import * as React from "react";
+import Link from "next/link";
+import Button1 from "@/components/Button/button1";
+import ArticleCard from "@/components/ArticleCard/ArticleCard";
+
+function MyComponent() {
+  return (
+    <div className="flex flex-col pb-16 bg-white">
+        <div 
+        style={{ 
+            fontFamily: 'Montserrat-Bold', 
+            fontSize: '20px', 
+        }}
+        className="flex justify-end pl-20 py-9 w-full font-bold whitespace-nowrap bg-white max-w-full max-md:px-5 max-md:max-w-full">
+            <div className="flex gap-5 justify-between w-full max-w-full max-md:flex-wrap max-md:max-w-full">
+                <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full">
+                <div className="flex-auto text-3xl tracking-normal leading-8 text-slate-800 mr-10">
+                    DiCheck
+                </div>
+                <div className="flex flex-auto gap-10 justify-start self-start text-sm tracking-wide leading-6 text-center text-neutral-500">
+                    <button className="justify-center py-2">Home</button>
+                    <button className="justify-center px-1.5 py-2">Check</button>
+                    <button className="justify-center py-2">Articles</button>
+                    <button className="justify-center px-1.5 py-2">History</button>
+                </div>
+                </div>
+                <div className="mr-40">
+                <Link href="/login">
+                    <Button1>
+                        Check now
+                    </Button1>
+                </Link>
+                </div>
+            </div>
+        </div>
+      <main className="flex flex-col px-14 mt-5 w-full max-md:px-5 max-md:mt-10 max-md:max-w-full">
+        <h2 
+        style={{ 
+            fontFamily: 'Montserrat-Bold', 
+        }} className="text-5xl font-bold text-gray-600 leading-[61.2px] max-md:max-w-full max-md:text-4xl">
+          Medical Article
+        </h2>
+        <section className="mt-10 flex flex-wrap justify-between max-md:max-w-full">
+            <div className="flex flex-wrap justify-between gap-5 max-md:flex-col max-md:gap-0">
+                <ArticleCard
+                date="04 June 2023"
+                title="Manfaat Olahraga Rutin untuk Kesehatan Mental"
+                description="Olahraga tidak hanya penting untuk kesehatan fisik, tetapi juga memiliki banyak manfaat untuk kesehatan mental."
+                imgSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/2dee867270ae73e565dda0a09101ddacdaec11a630a6286647269c3e2c8be9a1?apiKey=7fd2b033b9574f39882fe9ef4728cd45&"
+                imgAlt="Manfaat Olahraga Rutin untuk Kesehatan Mental"
+                href="/articles/article-1"
+                />
+                <ArticleCard
+                date="03 June 2023"
+                title="Pentingnya Vaksinasi dalam Mencegah Penyakit Menular"
+                description="Vaksinasi adalah salah satu intervensi kesehatan masyarakat yang paling efektif dalam mencegah penyebaran penyakit menular."
+                imgSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/4b4d2fe1e80d88dc03b72422c7d0bfd916ddc898ce2338bdc27f9e027a34adaf?apiKey=7fd2b033b9574f39882fe9ef4728cd45&"
+                imgAlt="Pentingnya Vaksinasi dalam Mencegah Penyakit Menular"
+                href="/articles/article-2"
+                />
+                <ArticleCard
+                date="01 June 2023"
+                title="Pentingnya Pola Makan Sehat untuk Mencegah Penyakit Kronis"
+                description="Pola makan sehat adalah kunci untuk mencegah berbagai penyakit kronis seperti diabetes, penyakit jantung, dan kanker."
+                imgSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/c191c7d43429783bafc4845f894dcb1b98bbb5e836b5bab44d114f0520d16df7?apiKey=7fd2b033b9574f39882fe9ef4728cd45&"
+                imgAlt="Pentingnya Pola Makan Sehat untuk Mencegah Penyakit Kronis"
+                href="/articles/article-3"
+                />
+            </div>
+            </section>
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/b1eda40223143c63f658c0b9012df51e224e21f410f2e96961b8700e97e94bb1?apiKey=7fd2b033b9574f39882fe9ef4728cd45&"
+          alt="Advertisement"
+          className="mt-11 max-w-full aspect-[3.45] w-[194px] max-md:mt-10"
+        />
+      </main>
+    </div>
+  );
+}
+
+export default MyComponent;

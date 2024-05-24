8e2d73e98848f10aa16e65adb7bc5375c774feb3
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2024-05-25 01:33:13 +0700

    feat: API logic for getRecordsByRecordId, getTotalRecordsByUserId, getTotalRecordsTodayByUserId, and getLatestRecordTime

--- backend/controllers/medicalRecordController.js
+++ backend/controllers/medicalRecordController.js
@@ -16,6 +16,19 @@ const getRecordsByUserId = async (req, res) => {
     }
 };
 
+const getRecordsByRecordId = async (req, res) => {
+    const { _id } = req.params;
+    try {
+        const recordList = await Record.find({ _id });
+        if (recordList.length === 0) {
+            return res.status(404).json({ error: "Tidak ada medical record yang ditemukan" });
+        }
+        res.status(200).json(recordList);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
 // Create Prediction
 const createRecord = async (req, res) => {
     const {user_id, symptomps, disease, treatment_advice, medicine_name, medicine_desc, record_date} = req.body;
@@ -44,4 +57,54 @@ const createRecord = async (req, res) => {
     }
 };
 
-module.exports = {getRecordsByUserId, createRecord};
+const getTotalRecordsByUserId = async (req, res) => {
+    const { user_id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(user_id)) {
+        return res.status(400).json({ error: 'Invalid user_id' });
+    }
+    try {
+        const recordCount = await Record.countDocuments({ user_id });
+        res.status(200).json({ totalRecords: recordCount });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const getTotalRecordsTodayByUserId = async (req, res) => {
+    const { user_id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(user_id)) {
+        return res.status(400).json({ error: 'Invalid user_id' });
+    }
+    try {
+        const start = new Date();
+        start.setHours(0,0,0,0);
+
+        const end = new Date();
+        end.setHours(23,59,59,999);
+
+        const recordCount = await Record.countDocuments({
+            user_id,
+            record_date: {
+                $gte: start,
+                $lt: end
+            }
+        });
+        res.status(200).json({ totalRecords: recordCount });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const getLatestRecordTime = async (req, res) => {
+    try {
+        const record = await Record.findOne().sort({record_date: -1});
+        if (!record) {
+            return res.status(404).json({ error: "No records found" });
+        }
+        res.status(200).json({ latestRecordTime: record.record_date });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+module.exports = {getRecordsByUserId, createRecord, getTotalRecordsByUserId, getTotalRecordsTodayByUserId, getLatestRecordTime, getRecordsByRecordId};

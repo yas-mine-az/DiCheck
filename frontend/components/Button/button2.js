df31b8610704750e989ff6b1495383be600f7c94
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2024-05-25 01:37:40 +0700

    feat: enhancement on button component

--- frontend/components/Button/button2.js
+++ frontend/components/Button/button2.js
@@ -5,7 +5,7 @@ const Button2 = ({ children, onClick }) => {
         style={{ 
           fontFamily: 'Montserrat-Bold'
         }}
-        className="justify-center px-10 py-4 text-sm font-bold tracking-wide leading-5 text-center text-gray-600 rounded-md border border-gray-600 border-solid"
+        className="hover:bg-gray-300 justify-center px-10 py-4 text-sm font-bold tracking-wide leading-5 text-center text-gray-600 rounded-md border border-gray-600 border-solid"
       >
         {children}
       </button>

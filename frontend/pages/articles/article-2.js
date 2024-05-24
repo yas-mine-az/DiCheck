7274004ced261eaa800eb0e83be90ce1f5d30dde
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2024-05-25 01:41:15 +0700

    feat: adjustmen & enhancement on article content pages

--- frontend/pages/articles/article-2.js
+++ frontend/pages/articles/article-2.js
@@ -80,14 +80,13 @@ export default function MyComponent() {
                             DiCheck
                         </div>
                         <div className="flex flex-auto gap-10 justify-start self-start text-sm tracking-wide leading-6 text-center text-neutral-500">
-                            <button className="justify-center py-2">Home</button>
-                            <button className="justify-center px-1.5 py-2">Check</button>
-                            <button className="justify-center py-2">Articles</button>
-                            <button className="justify-center px-1.5 py-2">History</button>
+                            <Link href="/homepage"> <button className="justify-center py-2">Home</button> </Link>
+                            <Link href="/check-disease"> <button className="justify-center px-1.5 py-2">Check</button> </Link>
+                            <Link href="/articles"> <button className="justify-center py-2">Articles</button> </Link>
                         </div>
                     </div>
                     <div className="mr-40">
-                        <Link href="/login">
+                        <Link href="/check-disease">
                             <Button1>
                                 Check now
                             </Button1>
